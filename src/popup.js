d8e5285734c6639a5642e64157b0fe584547b2a6
owen <on-hill@outlook.com>
2023-12-18 00:49:57 -0600

    incorporate chrome.storage for loading/saving settings

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,22 @@
 const removeFeed = document.getElementById('remove-feed');
 const currentSettings = { 'removeFeed': false };
 
+document.addEventListener('DOMContentLoaded', loadSettings(currentSettings));
+
 removeFeed.addEventListener('click', () => {
   currentSettings['removeFeed'] = removeFeed.checked;
   updateSettings(currentSettings)
 })
 
 function updateSettings(newSettings) {
+  chrome.storage.sync.set(newSettings, () => {})
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {settings: newSettings})
+  })
+}
+
+function loadSettings() {
+  chrome.storage.sync.get(currentSettings, (settings) => {
+    removeFeed.checked = settings['removeFeed'];
   })
 }
