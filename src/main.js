29c04e5eeed7fdb496c15aa577edc92ad2a2a47f
owen <on-hill@outlook.com>
2023-12-18 22:07:18 -0600

    develop and integrate removeSuggestedPosts functionallty

--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
-let currentSettings = { "removeFeed": false };
+let currentSettings = { 'removeFeed': false, 'removeSuggestedPosts': false };
 const feedPathName = '/feed/'
-
 chrome.runtime.onMessage.addListener((req, send, reply) => {
   currentSettings = req['settings'];
   applySettings(currentSettings);
 })
 
 function applySettings(newSettings) {
-  removeFeed(newSettings["removeFeed"])
+  removeFeed(newSettings['removeFeed'])
+  removeSuggestedPosts(newSettings['removeSuggestedPosts'])
 }
 
 function removeFeed(shouldRemove) {
   if (shouldRemove && feedPathName == window.location.pathname) {
     const feed = document.getElementsByClassName('scaffold-finite-scroll');
     if (feed[0]) { feed[0].remove(); }
+  }
+}
+
+function removeSuggestedPosts(shouldRemove) {
+  if (shouldRemove && feedPathName == window.location.pathname) {
+    const allSpecialPosts = [...document.getElementsByClassName('update-components-header__text-view')];
+    if (allSpecialPosts.length) {
+      allSpecialPosts.map((textElement) => {
+        if (textElement.innerText === "Suggested") textElement.closest('.full-height').remove()
+      })
+    }
   }
 }
