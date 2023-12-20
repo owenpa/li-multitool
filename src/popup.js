e41a3071ee45978cb9700d417117cbbbef18e86e
owen <on-hill@outlook.com>
2023-12-20 12:08:02 -0600

    remove commented code

--- src/popup.js
+++ src/popup.js
@@ -1,64 +1,27 @@
-// const removeFeed = document.getElementById('remove-feed');
-// const removeSuggestedPosts = document.getElementById('remove-suggested-posts');
-// const removeAds = document.getElementById('remove-ads');
-// const removeCelebrations = document.getElementById('remove-celebrations');
-// const debloatMyNetwork = document.getElementById('debloat-mynetwork');
-// const removePremium = document.getElementById('remove-premium');
-const currentSettings = { 'remove-feed': false, 'remove-suggested-posts': false, 'remove-ads': false, 'remove-celebrations': false, 'debloat-mynetwork': false, 'remove-premium': false };
+const settings = {};
 
-document.addEventListener('DOMContentLoaded', loadSettings(currentSettings));
+document.addEventListener('DOMContentLoaded', loadSettings());
 
-document.forms['options'].addEventListener('change', (toggledElement) => {
-  // toggledElement.target.id
-  // toggledElement.target.checked
+document.forms['options'].addEventListener('change', () => {
+  Array.from(document.forms['options']).map((optionElement) => {
+    settings[optionElement.id] = optionElement.checked;
+  });
+  updateSettings(settings);
 })
 
-// removeFeed.addEventListener('click', () => {
-//   const x = document.forms;
-//   console.log(x);
-//   currentSettings['removeFeed'] = removeFeed.checked;
-//   updateSettings(currentSettings);
-// })
 
-// removeSuggestedPosts.addEventListener('click', () => {
-//   currentSettings['removeSuggestedPosts'] = removeSuggestedPosts.checked;
-//   updateSettings(currentSettings);
-// })
-
-// removeAds.addEventListener('click', () => {
-//   currentSettings['removeAds'] = removeAds.checked;
-//   updateSettings(currentSettings);
-// })
-
-// removeCelebrations.addEventListener('click', () => {
-//   currentSettings['removeCelebrations'] = removeCelebrations.checked;
-//   updateSettings(currentSettings);
-// })
-
-// debloatMyNetwork.addEventListener('click', () => {
-//   currentSettings['debloatMyNetwork'] = debloatMyNetwork.checked;
-//   updateSettings(currentSettings);
-// })
-
-// removePremium.addEventListener('click', () => {
-//   currentSettings['removePremium'] = removePremium.checked;
-//   updateSettings(currentSettings);
-//  });
-
-function updateSettings(newSettings) {
-  chrome.storage.sync.set(newSettings, () => {})
+async function updateSettings(newSettings) {
+  chrome.storage.sync.set({ 'settings': newSettings }).then(() => loadSettings())
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { settings: newSettings });
+    chrome.tabs.sendMessage(tabs[0].id, { 'settings': newSettings });
   })
 }
 
 function loadSettings() {
-  chrome.storage.sync.get(currentSettings, (settings) => {
-    removeFeed.checked = settings['removeFeed'];
-    removeSuggestedPosts.checked = settings['removeSuggestedPosts'];
-    removeAds.checked = settings['removeAds'];
-    removeCelebrations.checked = settings['removeCelebrations'];
-    debloatMyNetwork.checked = settings['debloatMyNetwork'];
-    removePremium.checked = settings['removePremium'];
+  chrome.storage.sync.get(['settings']).then((chromeStoredSettings) => {
+    const extensionSettings = chromeStoredSettings['settings'];
+    for (let key in extensionSettings) {
+      document.forms['options'][key].checked = extensionSettings[key];
+    }
   })
 }
